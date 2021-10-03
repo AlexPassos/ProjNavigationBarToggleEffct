6641642b2ffaf132ec4d3a01150657770ab3b1bd
Alex Passos <alexpassosribeiro@gmail.com>
2021-10-03 20:22:47 -0300

    Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,23 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'ProjNavigationBarToggleEffct';
+
+  bActive: boolean = false;
+  cAtiveButton: string = '';
+  cAtiveNav: string = '';
+
+  fnMenu() {
+    if (this.bActive) {
+      this.cAtiveButton = '';
+      this.cAtiveNav = '';
+    } else {
+      this.cAtiveButton = 'fa-times';
+      this.cAtiveNav = 'active';
+    }
+    this.bActive = !this.bActive;
+  }
 }
